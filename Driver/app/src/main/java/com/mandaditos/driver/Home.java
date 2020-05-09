321c5ddb7c3c79fe167b90060038fc3ee263815b
floresblanco25@gmail.com <floresblanco25@gmail.com>
2020-05-08 22:26:29 -0600

    uodate

--- Driver/app/src/main/java/com/mandaditos/driver/Home.java
+++ Driver/app/src/main/java/com/mandaditos/driver/Home.java
@@ -8,6 +8,10 @@ import android.net.*;
 import android.os.*;
 import android.support.annotation.*;
 import android.support.v7.widget.*;
+import android.util.*;
+import android.view.*;
+import android.view.View.*;
+import android.widget.*;
 import com.google.android.gms.maps.model.*;
 import com.google.firebase.auth.*;
 import com.google.firebase.database.*;
@@ -16,9 +20,6 @@ import com.mandaditos.driver.models.*;
 import java.util.*;
 
 import android.Manifest;
-import android.widget.*;
-import android.view.View.*;
-import android.view.*;
 
 public class Home extends Activity 
 {
@@ -27,6 +28,7 @@ public class Home extends Activity
 	private String uId;
 	private FirebaseAuth mFirebaseAuth;
 	private Button logout;
+	private TextView BienvenidoDriverTv;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -35,6 +37,11 @@ public class Home extends Activity
 		mDataBase = FirebaseDatabase.getInstance().getReference("Ordenes");
 		mFirebaseAuth = FirebaseAuth.getInstance();
 		logout = findViewById(R.id.CerrarSesionmainButton1);
+		BienvenidoDriverTv = findViewById(R.id.BienvenidomainTextView);
+		
+		
+		
+		
 		logout.setOnClickListener(new OnClickListener(){
 
 				@Override
@@ -51,6 +58,40 @@ public class Home extends Activity
 		mFirebaseAuth = FirebaseAuth.getInstance();
 		FirebaseUser mFirebaseUser = mFirebaseAuth.getCurrentUser();
 		uId = mFirebaseUser.getUid().toString();
+		
+		
+//aqui obtenemos el nombre del usuario
+		mFirebaseAuth = FirebaseAuth.getInstance();
+		String userId = mFirebaseUser.getUid().toString();
+		DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Drivers/" + userId + "/Perfil").child("nombre");
+		ref.addListenerForSingleValueEvent(new ValueEventListener() {
+				@Override
+				public void onDataChange(DataSnapshot dataSnapshot) {
+					String t = dataSnapshot.getValue(String.class);
+					setNombreUsuario(t);
+
+				}
+				private void setNombreUsuario(String t)
+				{
+
+					String usuario = t;
+					try{
+						int index = usuario.indexOf(' ');
+						usuario = usuario.substring(0, index);
+					}catch(Exception e){Log.e("Couldent split name",e.toString());}
+
+
+					BienvenidoDriverTv.setText("Bienvenido "+usuario+"");
+
+				}
+
+				@Override
+				public void onCancelled(DatabaseError databaseError) {
+
+				}
+			});
+
+		
 			
 			
 		mDataBase.addValueEventListener(new ValueEventListener(){
@@ -90,11 +131,11 @@ public class Home extends Activity
 							m.setLatLngB(new LatLng(latB,lngB));
 							m.setNumeroDeOrden(postSnapshot.getKey().toString());
 							m.setDriverUid(postSnapshot.child("driverAsignado").getValue().toString());
-//							if(m.getEstadoDeOrden().toString().toLowerCase().matches("Sin completar".toLowerCase())){
+							if(m.getEstadoDeOrden().toString().toLowerCase().matches("Sin completar".toLowerCase())){
 								if(m.getDriverUid().toString().matches(uId)){
 									ordersList.add(m);
 									}
-//							}
+							}
 						}
 
 
@@ -116,8 +157,138 @@ public class Home extends Activity
 
 
 	}
+	
+	public void mostrarCompletadas(View v) {
+		mDataBase.addValueEventListener(new ValueEventListener(){
 
 
+				@Override
+				public void onDataChange(DataSnapshot p1)
+				{
+					try {
+						Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+						Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
+						r.play();
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+					if(p1.exists()){
+
+						List<mandaditosModel> ordersList = new ArrayList<mandaditosModel>();
+						for (DataSnapshot postSnapshot : p1.getChildren()) {
+							double latA = postSnapshot.child("latLngA/latitude").getValue();
+							double lngA = postSnapshot.child("latLngA/longitude").getValue();
+							double latB = postSnapshot.child("latLngB/latitude").getValue();
+							double lngB = postSnapshot.child("latLngB/longitude").getValue();
+
+							mandaditosModel m = new mandaditosModel();
+							m.setUserId(postSnapshot.child("userId").getValue().toString());
+							m.setUsuario(postSnapshot.child("usuario").getValue().toString());
+							m.setPartida(postSnapshot.child("partida").getValue().toString());
+							m.setDestino(postSnapshot.child("destino").getValue().toString());
+							m.setDistancia(postSnapshot.child("distancia").getValue().toString());
+							m.setFecha(postSnapshot.child("fecha").getValue().toString());
+							m.setETA(postSnapshot.child("eta").getValue().toString());
+							m.setRecogerDineroEn(postSnapshot.child("recogerDineroEn").getValue().toString());
+							m.setCosto(postSnapshot.child("costo").getValue().toString());
+							m.setEstadoDeOrden(postSnapshot.child("estadoDeOrden").getValue().toString());
+							m.setLatLngA(new LatLng(latA,lngA));
+							m.setLatLngB(new LatLng(latB,lngB));
+							m.setNumeroDeOrden(postSnapshot.getKey().toString());
+							m.setDriverUid(postSnapshot.child("driverAsignado").getValue().toString());
+							if(m.getEstadoDeOrden().toString().toLowerCase().matches("Completada".toLowerCase())){
+								if(m.getDriverUid().toString().matches(uId)){
+									ordersList.add(m);
+								}
+							}
+						}
+
+
+						mAdapter adapter = new mAdapter(Home.this,ordersList);
+						RecyclerView mRecyclerView = findViewById(R.id.home_recycler);
+						mRecyclerView.setHasFixedSize(true);
+						LinearLayoutManager layoutManager = new LinearLayoutManager(Home.this);
+						layoutManager.setReverseLayout(false);
+						layoutManager.setStackFromEnd(false);
+						mRecyclerView.setLayoutManager(layoutManager);
+						mRecyclerView.setAdapter(adapter);
+					}
+					else{}
+				}
+				@Override
+				public void onCancelled(DatabaseError p1){
+				}
+			});
+		// does something very interesting
+	}
+	public void mostrarSinCompletar(View v) {
+		
+		mDataBase.addValueEventListener(new ValueEventListener(){
+
+
+				@Override
+				public void onDataChange(DataSnapshot p1)
+				{
+					try {
+						Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+						Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
+						r.play();
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+					if(p1.exists()){
+
+						List<mandaditosModel> ordersList = new ArrayList<mandaditosModel>();
+						for (DataSnapshot postSnapshot : p1.getChildren()) {
+							double latA = postSnapshot.child("latLngA/latitude").getValue();
+							double lngA = postSnapshot.child("latLngA/longitude").getValue();
+							double latB = postSnapshot.child("latLngB/latitude").getValue();
+							double lngB = postSnapshot.child("latLngB/longitude").getValue();
+
+							mandaditosModel m = new mandaditosModel();
+							m.setUserId(postSnapshot.child("userId").getValue().toString());
+							m.setUsuario(postSnapshot.child("usuario").getValue().toString());
+							m.setPartida(postSnapshot.child("partida").getValue().toString());
+							m.setDestino(postSnapshot.child("destino").getValue().toString());
+							m.setDistancia(postSnapshot.child("distancia").getValue().toString());
+							m.setFecha(postSnapshot.child("fecha").getValue().toString());
+							m.setETA(postSnapshot.child("eta").getValue().toString());
+							m.setRecogerDineroEn(postSnapshot.child("recogerDineroEn").getValue().toString());
+							m.setCosto(postSnapshot.child("costo").getValue().toString());
+							m.setEstadoDeOrden(postSnapshot.child("estadoDeOrden").getValue().toString());
+							m.setLatLngA(new LatLng(latA,lngA));
+							m.setLatLngB(new LatLng(latB,lngB));
+							m.setNumeroDeOrden(postSnapshot.getKey().toString());
+							m.setDriverUid(postSnapshot.child("driverAsignado").getValue().toString());
+							if(m.getEstadoDeOrden().toString().toLowerCase().matches("Sin completar".toLowerCase())){
+								if(m.getDriverUid().toString().matches(uId)){
+									ordersList.add(m);
+								}
+							}
+						}
+
+
+						mAdapter adapter = new mAdapter(Home.this,ordersList);
+						RecyclerView mRecyclerView = findViewById(R.id.home_recycler);
+						mRecyclerView.setHasFixedSize(true);
+						LinearLayoutManager layoutManager = new LinearLayoutManager(Home.this);
+						layoutManager.setReverseLayout(false);
+						layoutManager.setStackFromEnd(false);
+						mRecyclerView.setLayoutManager(layoutManager);
+						mRecyclerView.setAdapter(adapter);
+					}
+					else{}
+				}
+				@Override
+				public void onCancelled(DatabaseError p1){
+				}
+			});
+		// does something very interesting
+	}
+
+	
+	
+	
 
 	@Override
 	public void onBackPressed() {
